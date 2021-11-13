c39ff790359ecb196b69473c89a025c20e769907
dewolaj881 <dewolaj881@gmail.com>
2021-11-13 03:03:59 +0100

    feat: extended tailwind color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-background': '#222',
+        'light-background': '#424242',
+      },
+    },
   },
   variants: {
     extend: {},
